4e3292b19f4602849582cddc948b79e4984b08d4
erickogi <erickogi14@gmail.com>
2017-09-18 11:19:13 +0300

    V1

---
+++ app/src/main/java/com/erickogi14gmail/odijos1/OdijoLFound/FragmentOdijoArrived.java
@@ -0,0 +1,10 @@
+package com.erickogi14gmail.odijos1.OdijoLFound;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Eric on 9/15/2017.
+ */
+
+public class FragmentOdijoArrived extends Fragment {
+}
